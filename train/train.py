d33ca82a44bb007deed9d9cdf67f755296586c76
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-04-26 14:26:41 +0200

    common

--- train/train.py
+++ train/train.py
@@ -1,87 +1,90 @@
 from dataset import Dataset
 from models.net_0.model import Model
-
-
-dataset = Dataset(96, 96, 100000, 1000)
-
-
-model = Model(dataset.training.input_shape, dataset.training.outputs_count)
-
 import torch
 
-epoch_count = 30
-learning_rates = [0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.00001]
+
+dataset = Dataset(96, 96, 100000, 10000)
+
+
+
 
 testing_loss_sum_best = None
 
-for epoch in range(epoch_count):
-    
-    batch_size  = 64
-    batch_count = (dataset.training.get_count()+batch_size) // batch_size
+nets_to_try = 100
 
-    learning_rate = learning_rates[epoch%len(learning_rates)]
-    
-    optimizer  = torch.optim.Adam(model.parameters(), lr= learning_rate, weight_decay=10**-6)  
+for net in range(nets_to_try):
 
-    training_loss_sum = 0.0
-    for batch_id in range(batch_count):
-        training_x, training_y = dataset.training.get_batch(batch_size)
+    epoch_count     = 20
+    learning_rates  = [0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.00001, 0.00001]
 
-        training_x = training_x.to(model.device)
-        training_y = training_y.to(model.device)
+    model = Model(dataset.training.input_shape, dataset.training.outputs_count)
 
-        predicted_y = model.forward(training_x)
+    for epoch in range(epoch_count):
+        
+        batch_size  = 32
+        batch_count = (dataset.training.get_count()+batch_size) // batch_size
 
-        error = (training_y - predicted_y)**2
-        loss  = error.mean() 
+        learning_rate = learning_rates[epoch%len(learning_rates)]
+        
+        optimizer  = torch.optim.Adam(model.parameters(), lr= learning_rate, weight_decay=10**-6)  
 
-        loss.backward()
-        optimizer.step()
+        training_loss_sum = 0.0
+        for batch_id in range(batch_count):
+            training_x, training_y = dataset.training.get_batch(batch_size)
 
-        training_loss_sum+= loss.detach().to("cpu").numpy()
+            training_x = training_x.to(model.device)
+            training_y = training_y.to(model.device)
 
-    training_loss_sum = training_loss_sum/batch_count
+            predicted_y = model.forward(training_x)
 
-    
-    batch_count = (dataset.testing.get_count()+batch_size) // batch_size
-    testing_loss_sum = 0.0
-    for batch_id in range(batch_count):
-        testing_x, testing_y = dataset.testing.get_batch(batch_size)
+            error = (training_y - predicted_y)**2
+            loss  = error.mean() 
 
-        testing_x = testing_x.to(model.device)
-        testing_y = testing_y.to(model.device)
+            loss.backward()
+            optimizer.step()
 
-        predicted_y = model.forward(testing_x)
+            training_loss_sum+= loss.detach().to("cpu").numpy()
 
-        error = (testing_y - predicted_y)**2
-        loss  = error.mean()
+        training_loss_sum = training_loss_sum/batch_count
 
-        testing_loss_sum+= loss.detach().to("cpu").numpy()
+        
+        batch_count = (dataset.testing.get_count()+batch_size) // batch_size
+        testing_loss_sum = 0.0
+        for batch_id in range(batch_count):
+            testing_x, testing_y = dataset.testing.get_batch(batch_size)
 
-    testing_loss_sum = testing_loss_sum/batch_count
+            testing_x = testing_x.to(model.device)
+            testing_y = testing_y.to(model.device)
 
+            predicted_y = model.forward(testing_x)
 
-    print("epoch = ", epoch, training_loss_sum, testing_loss_sum)
-    
-    save_model = False
-    
-    if testing_loss_sum_best == None:
-        testing_loss_sum_best = testing_loss_sum
-        save_model = True
+            error = (testing_y - predicted_y)**2
+            loss  = error.mean()
 
-    if testing_loss_sum < testing_loss_sum_best:
-        testing_loss_sum_best = testing_loss_sum
-        save_model = True
+            testing_loss_sum+= loss.detach().to("cpu").numpy()
+
+        testing_loss_sum = testing_loss_sum/batch_count
 
 
-    if save_model:
-        model.save("models/net_0/")
+        #print("epoch = ", epoch, training_loss_sum, testing_loss_sum)
+        
+        save_model = False
+        
+        if testing_loss_sum_best == None:
+            testing_loss_sum_best = testing_loss_sum
+            save_model = True
 
-        print("\n\n\n")
-        print("=================================================")
-        print("new best net in ", epoch, "\n")
-        print("TRAINING result ")
-        print(training_loss_sum)
-        print("TESTING result ")
-        print(testing_loss_sum)
-        print("\n\n\n")
+        if testing_loss_sum < testing_loss_sum_best:
+            testing_loss_sum_best = testing_loss_sum
+            save_model = True
+
+
+        if save_model:
+            model.save("models/net_0/")
+
+            print("\n\n\n")
+            print("net ID = ", net)
+            print("new best net in ", epoch, "\n")
+            print("TRAINING result ", training_loss_sum)
+            print("TESTING result ", testing_loss_sum)
+            print("\n\n\n")
