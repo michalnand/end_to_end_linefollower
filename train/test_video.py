77a692ac40c6fd1374b0819453dccc90587d968a
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-04-30 12:39:22 +0200

    video tests with segmentation

--- train/test_video.py
+++ train/test_video.py
@@ -5,23 +5,24 @@ import time
 from models.net_1.model import Model
 
 
-model_input_width  = 96*4
-model_input_height = 96*4
+model_input_width  = 512 #96
+model_input_height = 512 #96
 
 model = Model((1, model_input_height, model_input_width), 2)
 model.load("models/net_1/")
 
+cap = cv2.VideoCapture("/Users/michal/Movies/line_path_video.mp4")
+input_width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+input_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-cap = cv2.VideoCapture(0)
 
-
+'''
+#cap = cv2.VideoCapture(0)
 input_width        = 640
 input_height       = 480
-
-
-
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,input_width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,input_height)
+'''
 
 def get_prediction(frame):
     frame_grayscale   = numpy.array(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
@@ -34,8 +35,10 @@ def get_prediction(frame):
 
     prediction_tensor  = model.forward(frame_tensor)
 
-    prediction_np      = numpy.clip(prediction_tensor[0][0].detach().to("cpu").numpy(), 0, 1)
+    prediction_np      = numpy.clip(prediction_tensor[0].detach().to("cpu").numpy(), 0, 1)
 
+    if prediction_tensor.shape[1] == 1:
+        prediction_np = prediction_np[0]
 
     return prediction_np
 
@@ -44,14 +47,22 @@ fps = 0.0
 
 frame_count = 0
 
-while(True):
+output = None
+#output = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 25, (input_width,input_height))
+
+
+while cap.isOpened():
     ret, frame      = cap.read()
+    if ret == False:
+        break
+
 
     time_start = time.time()
     prediction_np   = get_prediction(frame)
     time_stop = time.time()
 
-    prediction_np   = cv2.resize(prediction_np, (input_width, input_height), interpolation = cv2.INTER_LANCZOS4)
+    prediction_np   = cv2.resize(prediction_np, (input_width, input_height), interpolation = cv2.INTER_CUBIC)
+    prediction_np   = numpy.clip(prediction_np, 0, 1)
 
     b, g, r = cv2.split(frame)
 
@@ -59,7 +70,12 @@ while(True):
     g = g/255.0
     b = b/255.0
 
-    g = 0.5*g  + 0.5*prediction_np
+    k = 0.4
+
+    r = (1.0 - k)*r 
+    g = (1.0 - k)*g  + k*prediction_np
+    b = (1.0 - k)*b
+
 
     r = (r*255).astype(dtype=numpy.uint8)
     g = (g*255).astype(dtype=numpy.uint8)
@@ -68,24 +84,28 @@ while(True):
     result = cv2.merge((b,g,r))
 
 
-    '''
-    frame_count+= 1
-    if frame_count%20 == 0:
-        cv2.imwrite("images/frame_" + str(frame_count) + ".jpg", result)
-    '''
+
+
+    k = 0.05
+    fps = (1.0 - k)*fps + k*1.0/(time_stop - time_start + 0.0000001)
+    text = "FPS = " + str(round(fps, 1))
+
+    result = cv2.putText(result, text, (30, 30), cv2.FONT_HERSHEY_SIMPLEX ,  1, (255, 0, 0), 2, cv2.LINE_AA) 
+
+    if output is not None:
+        output.write(result)
 
     cv2.imshow('frame', result)
 
-    fps = 0.9*fps + 0.1*1.0/(time_stop - time_start)
-
-    print("FPS = ", round(fps, 1))
+    print(text)
 
 
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
-# When everything done, release the capture
 cap.release()
+if output is not None:
+    out.release()
 cv2.destroyAllWindows()
 
